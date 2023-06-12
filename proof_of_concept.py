73c9d7de0c46c49c6ed750b6317b307df8153cc4
bdiamond2 <benarthur.diamond@gmail.com>
2023-06-12 04:41:24 +0000

    comparison proof of concept

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,9 +1,12 @@
 import pyheif
 import piexif
+from datetime import datetime
+import os
 import pprint
+import math
 
 # takes the path of an HEIC image and returns the datetime and geotag
-# as a lat/long
+# as a lat/long in a list
 def heic_dt_gps(image_path):
     # Open the HEIC image using pyheif
     heif_file = pyheif.read(image_path)
@@ -12,13 +15,13 @@ def heic_dt_gps(image_path):
     metadata = heif_file.metadata
     exif_data_bytes = metadata[0]['data']
     exif_dict = piexif.load(exif_data_bytes)
-    # pprint.pprint(exif_dict)
 
     # EXIF reference:
     # https://exiv2.org/tags.html
-    dt = exif_dict['0th'][306].decode('utf-8')
+    # dt = exif_dict['0th'][306].decode('utf-8')
+    dt = datetime.strptime(exif_dict['0th'][306].decode('utf-8'), "%Y:%m:%d %H:%M:%S")
     lat_long = decode_gps(exif_dict['GPS'])
-    print(dt, lat_long)
+    return [dt, lat_long]
 
 # takes the 'GPS' tag of EXIF data and returns it as a simple
 # decimal lat/long list
@@ -47,7 +50,40 @@ def decode_gps(gps_tag):
 def exif_dms_to_dec(dms):
     return (dms[0][0] / dms[0][1]) + (dms[1][0] / dms[1][1])/60 + (dms[2][0] / dms[2][1])/3600
 
+def list_files(path):
+    fns = []
+    for fn in os.listdir(path):
+        fns.append(os.path.join(path, fn))
+    return fns
+
 img = "IMG_7589.HEIC"
 
-# Extract geotag metadata from the image
-heic_dt_gps(img)
+batch1 = list_files('test_heics/batch_1')
+batch2 = list_files('test_heics/batch_2')
+
+batch1_data = {}
+for heic in batch1:
+    batch1_data[heic] = heic_dt_gps(heic)
+
+batch2_data = {}
+for heic in batch2:
+    batch2_data[heic] = heic_dt_gps(heic)
+
+comp = {}
+deg_to_m = 111139
+for img1 in batch1_data:
+    comp[img1] = {}
+    for img2 in batch2_data:
+        tdelta = (batch1_data[img1][0] - batch2_data[img2][0]).total_seconds()
+        p1 = batch1_data[img1][1]
+        p2 = batch2_data[img2][1]
+        dist = deg_to_m * math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+        comp[img1][img2] = [tdelta, dist]
+
+pprint.pprint(comp)
+
+# pprint.pprint('batch 1:')
+# pprint.pprint(batch_1_data)
+
+# pprint.pprint('\nbatch 2:')
+# pprint.pprint(batch_2_data)
